c5f8de5a9f5bc2c0802f65844ddb4bb2a7498770
ct123098 <chen-t18@mails.tsinghua.edu.cn>
2020-09-29 12:25:48 +0800

    PA1: finished sphere

--- PA1-RayCasting/code/include/sphere.hpp
+++ PA1-RayCasting/code/include/sphere.hpp
@@ -13,19 +13,35 @@ public:
         // unit ball at the center
     }
 
-    Sphere(const Vector3f &center, float radius, Material *material) : Object3D(material) {
-        // 
+    Sphere(const Vector3f &center, float radius, Material *_material) : Object3D(material) {
+        o = center;
+        r = radius;
     }
 
     ~Sphere() override = default;
 
-    bool intersect(const Ray &r, Hit &h, float tmin) override {
-        //
-        return false;
+    bool intersect(const Ray &ray, Hit &hit, float tmin) override {
+        Vector3f a = ray.getOrigin(), d = ray.getDirection();
+        // cerr << a[0] << ' ' << a[1] << ' ' << a[2] << ' ' << d[0] << ' ' << d[1] << ' '<< d[2] << endl;
+        Vector3f ao = o - a;
+        double l = Vector3f::dot(ao, d), ao2 = Vector3f::dot(ao, ao);
+        // cerr << l << ' ' << ao2 << endl;
+        double h2 = ao2 - l * l;
+        // cerr << h2 << ' ' << r * r << endl;
+        if (h2 > r * r) {
+            return false;
+        }
+        double ll = l - sqrt(r * r - h2);
+        Vector3f point = a + ll * d, normal = point - o;
+        if (hit.getT() > ll) {
+            hit.set(ll, material, normal);
+        }
+        return true;
     }
 
 protected:
-
+    Vector3f o;
+    double r;
 };
 
 
