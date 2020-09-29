0f324eb8e1d3139c1554cc5aa2f3cab02cd76c68
ct123098 <chen-t18@mails.tsinghua.edu.cn>
2020-09-29 10:26:14 +0800

    PA1: finish group

--- PA1-RayCasting/code/include/group.hpp
+++ PA1-RayCasting/code/include/group.hpp
@@ -19,7 +19,7 @@ public:
     }
 
     explicit Group (int num_objects) {
-
+        vec.resize(num_obsjects);
     }
 
     ~Group() override {
@@ -27,19 +27,22 @@ public:
     }
 
     bool intersect(const Ray &r, Hit &h, float tmin) override {
-
+        for (int i = 0; i < vec.size(); i++)
+            if (vec[i] != nullptr) {
+                vec[i]->intersect(r, h, tmin);
+            }
     }
 
     void addObject(int index, Object3D *obj) {
-
+        vec[index] = obs;
     }
 
     int getGroupSize() {
-
+        return vec.size();
     }
 
 private:
-
+    vector<Object3D*> vec;
 };
 
 #endif
