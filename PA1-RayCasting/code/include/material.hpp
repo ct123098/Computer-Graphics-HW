136828da31ef98fcd7b4f5a171878239e86ab094
ct123098 <chen-t18@mails.tsinghua.edu.cn>
2020-09-29 16:14:47 +0800

    PA1: finished material

--- PA1-RayCasting/code/include/material.hpp
+++ PA1-RayCasting/code/include/material.hpp
@@ -14,7 +14,6 @@ public:
 
     explicit Material(const Vector3f &d_color, const Vector3f &s_color = Vector3f::ZERO, float s = 0) :
             diffuseColor(d_color), specularColor(s_color), shininess(s) {
-
     }
 
     virtual ~Material() = default;
@@ -23,11 +22,18 @@ public:
         return diffuseColor;
     }
 
+    static double clip(double x, double mn = 0.0, double mx = 1.0) {
+        return x > mx ? mx : x < mn ? mn : x;
+    }
 
     Vector3f Shade(const Ray &ray, const Hit &hit,
                    const Vector3f &dirToLight, const Vector3f &lightColor) {
         Vector3f shaded = Vector3f::ZERO;
-        // 
+        Vector3f N = hit.getNormal(), L = dirToLight, V = -ray.getDirection();
+        Vector3f R = 2 * Vector3f::dot(N, L) * N - L;
+        // N.print(), L.print(), V.print(), R.print();
+        shaded += lightColor * diffuseColor * clip(Vector3f::dot(N, L));
+        shaded += lightColor * specularColor * pow(clip(Vector3f::dot(V, R)), shininess);
         return shaded;
     }
 
