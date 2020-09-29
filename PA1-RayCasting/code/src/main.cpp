228c0a0b906f58d81848360a443e99a2157b89ed
ct123098 <chen-t18@mails.tsinghua.edu.cn>
2020-09-29 18:36:33 +0800

    PA1: update main

--- PA1-RayCasting/code/src/main.cpp
+++ PA1-RayCasting/code/src/main.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <cmath>
 #include <iostream>
+#include <ctime>
 
 #include "scene_parser.hpp"
 #include "image.hpp"
@@ -14,6 +15,13 @@
 #include <string>
 
 using namespace std;
+
+Vector3f clip_vec(const Vector3f &a, double mn = 0.0, double mx = 1.0) {
+    Vector3f ret = a;
+    for (int i = 0; i < 3; i++)
+        ret[i] = a[i] > mx ? mx : a[i] < mn ? mn : a[i];
+    return ret;
+}
 
 int main(int argc, char *argv[]) {
     for (int argNum = 1; argNum < argc; ++argNum) {
@@ -34,7 +42,10 @@ int main(int argc, char *argv[]) {
     // the scene.  Write the color at the intersection to that
     // pixel in your output image.
     // cout << "Hello! Computer Graphics!" << endl;
+    
     cout << "Rendering started." << endl;
+    std::clock_t c_start = std::clock();
+
     SceneParser *sceneParser = new SceneParser(inputFile.c_str());
     Camera *camera = sceneParser->getCamera();
     int w = camera->getWidth(), h = camera->getHeight();
@@ -42,6 +53,7 @@ int main(int argc, char *argv[]) {
 
     for (int x = 0; x < camera->getWidth(); x++)
         for (int y = 0; y < camera->getHeight(); y++) {
+            // if (x != 100 || y != 66) continue;
             Ray camRay = camera->generateRay(Vector2f(x, y));
             Group *baseGroup = sceneParser->getGroup();
             Hit hit;
@@ -54,14 +66,19 @@ int main(int argc, char *argv[]) {
                     light->getIllumination(camRay.pointAtParameter(hit.getT()), L, lightColor);
                     finalColor += hit.getMaterial()->Shade(camRay, hit, L, lightColor);
                 }
-                image->SetPixel(x, y, finalColor);
+                image->SetPixel(x, y, clip_vec(finalColor));
+                // image->SetPixel(x, y, 1.0 - sceneParser->getBackgroundColor());
             }
             else {
                 image->SetPixel(x, y, sceneParser->getBackgroundColor());
             }
         }
+    EXIT:;
     image->SaveImage(outputFile.c_str());
-    cout << "Rendering finished." << endl;
+    
+    std::clock_t c_end = std::clock();
+    cout << "Rendering finished. ";
+    cout << "(CPU time used: " << 1000.0 * (c_end-c_start) / CLOCKS_PER_SEC <<"ms)" << endl;
     return 0;
 }
 
