6ff369d77e4eb7187188943bbb02f0f281a0a56d
ct123098 <chen-t18@mails.tsinghua.edu.cn>
2020-09-29 23:22:58 +0800

    PA1: update

--- PA1-RayCasting/code/run_all.sh
+++ PA1-RayCasting/code/run_all.sh
@@ -16,7 +16,7 @@ cd ..
 # Run all testcases. 
 # You can comment some lines to disable the run of specific examples.
 mkdir -p output
-bin/PA1 testcases/test.txt output/test.bmp
+# bin/PA1 testcases/test.txt output/test.bmp
 bin/PA1 testcases/scene01_basic.txt output/scene01.bmp
 bin/PA1 testcases/scene02_cube.txt output/scene02.bmp
 bin/PA1 testcases/scene03_sphere.txt output/scene03.bmp
