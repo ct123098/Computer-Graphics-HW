094f109dd2dd28f6c646ea37b6298866a6d1b30f
ct123098 <chen-t18@mails.tsinghua.edu.cn>
2020-09-29 16:14:35 +0800

    PA1: add rectangle support

--- PA1-RayCasting/code/include/camera.hpp
+++ PA1-RayCasting/code/include/camera.hpp
@@ -44,15 +44,17 @@ public:
     PerspectiveCamera(const Vector3f &center, const Vector3f &direction,
             const Vector3f &up, int imgW, int imgH, float angle) : Camera(center, direction, up, imgW, imgH) {
         // angle is in radian.
-        double t = (2 * tan(angle / 2));
-        fx = width / t, fy = height / t;
+        double t = (2 * tan(angle / 2)), f = std::min(width, height) / t;
+        // fx = width / t, fy = height / t; // make both edge fit the angle
+        fx = f, fy = f; // make the shorter edge fit the angle
         cx = width / 2.0, cy = height / 2.0;
     }
 
     Ray generateRay(const Vector2f &point) override {
         Vector3f d0((point.x() - cx) / fx, (point.y() - cy) / fy, -1.0);
-        Matrix3f R(horizontal, up, direction);
-        Ray r(center, (R * d0).normalized());
+        Matrix3f R(horizontal, up, -direction);
+        Ray ret(center, (R * d0).normalized());
+        return ret;
     }
 
 protected:
